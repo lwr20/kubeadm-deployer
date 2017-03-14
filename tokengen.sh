08e34c8d6a4d451a7a2d40ed3a7f5eb286b94744
Lance Robson <lance@tigera.io>
2017-03-14 17:39:15 +0000

    Ensure tokengen always gives the right length token

--- tokengen.sh
+++ tokengen.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python -c 'import random; print "%0x.%0x" % (random.SystemRandom().getrandbits(3*8), random.SystemRandom().getrandbits(8*8))'
+python -c 'import random; print "%6x.%16x" % (random.SystemRandom().getrandbits(3*8), random.SystemRandom().getrandbits(8*8))'
